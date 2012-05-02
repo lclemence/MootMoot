078c707baf2d61c7ca945a2457f3fb89163bfcf0
Clemence Lelong <lelongclemence@gmail.com>
2012-05-02 14:49:05 +1200

    create categorizations (relation between pictures and galleries)

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,5 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name
-  has_many :pictures
+  has_many :categorizations
+  has_many :pictures , :through => :categorizations
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
-  belongs_to :gallery
-  attr_accessible :caption, :title, :url, :gallery_id, :thumb_url
+  has_many :gallery , :through => :categorizations
+  has_many :categorizations
+  attr_accessible :caption, :title, :url, :thumb_url
 end
