958cf4ccfec474e60328ca50d485049e73b4a719
Clemence Lelong <lelongclemence@gmail.com>
2012-05-17 11:20:29 +1200

    edit params

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,4 +3,13 @@ class AdminController < ApplicationController
 
   def index
   end
+  
+  def editparams
+     constants = params[:value_constants]
+     Constant.delete_all
+     constants.each do |k,v|
+       Constant.create(:name => k, :value => v.first)
+     end
+     redirect_to admin_path
+  end
 end
