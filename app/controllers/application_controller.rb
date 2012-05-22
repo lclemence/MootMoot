870225249521959c12584dfe28ada10eb9486df6
Julien Pellet <pelletj@gmail.com>
2012-05-22 16:49:50 +1200

    add fr locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   layout :layout_by_role
-
+   
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
 
   private
