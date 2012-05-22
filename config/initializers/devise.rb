cec6e3e599059891e2fb8babefae141c23e60658
Julien Pellet <pelletj@gmail.com>
2012-05-23 09:45:49 +1200

    add a bit more awesomeness to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "webmaster@julienpellet.com"
+  config.mailer_sender = "julien-noreply@julienpellet.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
