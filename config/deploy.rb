4885be700975f4f694f534f45beed60f102b1d86
Julien Pellet <pelletj@gmail.com>
2012-05-28 14:16:00 +1200

    add more rails awesomeness in configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -98,7 +98,12 @@ namespace :apache2 do
 <VirtualHost *:80>
    ServerName #{vhost_hostname}
    ServerAlias #{vhost_hostname}
+
+   PassengerMinInstances 3
+   PassengerPreStart #{vhost_hostname}
+
    DocumentRoot #{current_path}/public/
+#   DefaultInitEnv RAILS_ENV production
    RackEnv #{rails_env}
    Options -MultiViews
    <Directory #{current_path}/public/>
@@ -107,13 +112,12 @@ namespace :apache2 do
       Order allow,deny
       Allow from all
    </Directory>
-  <LocationMatch "^/assets/.*$">
-#    # 1 year cache for assets -- requires mod_expires
-#    FileETag None
-#    # RFC says only cache for 1 year
-#    ExpiresActive On
-#    ExpiresDefault "access plus 1 year"
-#  </LocationMatch>
+    # 1 year cache for assets -- requires mod_expires
+    FileETag None
+    # RFC says only cache for 1 year
+    ExpiresActive On
+    ExpiresDefault "access plus 1 year"
+  </LocationMatch>
 </VirtualHost>
 EOF
     put template, "/etc/apache2/sites-enabled/#{application}-#{rails_env}_vhost.conf"
