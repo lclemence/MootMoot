d5f560dce65605c77363ab4ccc00006c499b8375
keyzz <pelletj@gmail.com>
2012-06-24 03:06:37 +1200

    order pictures in galleries

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name
   has_many :categorizations
-  has_many :pictures , :through => :categorizations
+  has_many :pictures , :through => :categorizations, :order => "created_at DESC"
 end
