8f96da3153704de52c312810d72ad9c057c297c3
keyzz <pelletj@gmail.com>
2012-09-26 22:03:21 +1000

    add auth user

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,7 @@
 class PicturesController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
-    redirect_to root_path, :anchor => "!#{params[:id]}"
+#    redirect_to root_path, :anchor => "!#{params[:id]}"
   end
 end
